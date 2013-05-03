dd9ba8a70cccfabcd77d9b567d5a5aadca8ab159
Rafael Canovas <rafaelcanovas@me.com>
2013-05-02 21:17:25 -0300

    New setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 version = '0.1.7.5'
 
-setup(name='mdx_video',
-    version=version,
-    description="Markdown 2.0 extension for easy video embedding",
-    author="Italo Maia",
-    url="https://github.com/italomaia/mdx-video",
-    py_modules = ["mdx_video"],
-    requires=['markdown (>=2.1)',],
+setup(
+	name='mdx_video',
+	version=version,
+	description="Markdown 2.0 extension for easy video embedding",
+	author="Tyler Lesmann, Italo Maia, Rafael Canovas",
+	url="https://github.com/mstrcnvs/mdx-video",
+	py_modules=["mdx_video"]
 )
