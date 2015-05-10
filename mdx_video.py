italomaia/mdx-video

commit cfaf96d8caadfeb0e226a1b9f034cbbc74245328

dont require configs argument

mdx_video.py
@@ -5,7 +5,7 @@ from markdown.util import etree
 
 
 class VideoExtension(markdown.Extension):
-    def __init__(self, configs):
+    def __init__(self, **kwargs):
         self.config = {
             'dailymotion_width': ['480', 'Width for Dailymotion videos'],
             'dailymotion_height': ['270', 'Height for Dailymotion videos'],
@@ -22,8 +22,9 @@ class VideoExtension(markdown.Extension):
         }
 
         # Override defaults with user settings
-        for key, value in configs:
-            self.setConfig(key, value)
+        if "configs" in kwargs:
+            for key, value in kwargs["configs"]:
+                self.setConfig(key, value)
 
     def add_inline(self, md, name, klass, re):
         pattern = klass(re)

commit 9c0fe9ad5a5c01f4196af54897a55269b99a516f

Fix default config loading.
Issue introduced in commit cfaf96d8caadfeb0e226a1b9f034cbbc74245328

mdx_video.py
@@ -22,7 +22,7 @@ class VideoExtension(markdown.Extension):
         }
 
         # Override defaults with user settings
-        if "configs" in kwargs:
+        if kwargs.get('configs', None):
             for key, value in kwargs["configs"]:
                 self.setConfig(key, value)
 